9dc0d65cb90afe7c548dea96bb284137d359e263
Edgar Andres Flores <eaflores@jerarquicos.com>
2026-02-24 23:18:15 -0300

    made downgrade version 8.0

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,7 +12,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+   
     app.UseSwagger();
     app.UseSwaggerUI();
 }
