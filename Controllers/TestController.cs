ce310cfc030e82e2d02a8fdb3e64ee3b6d71204e
Edgar Andres Flores <eaflores@jerarquicos.com>
2026-02-26 18:35:08 -0300

    added TestController just for debugging ci/cd cycle

---
+++ Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DirtyApi.Controllers
+{
+    public class TestController : ControllerBase
+    {
+        [HttpGet("/api/v1/test")]
+        public IActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
